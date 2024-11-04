9e00c4d022292c4c1cd29caf5504281346af5c40
lamnmph <minhlam021104@gmail.com>
2024-11-04 20:31:10 +0700

    admin

---
+++ client/common/env.php
@@ -0,0 +1,9 @@
+<?php
+define('BASE_URL', 'http://localhost/client/client-page/');
+
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+
+define('PATH_ROOT', __DIR__ . '/../');
