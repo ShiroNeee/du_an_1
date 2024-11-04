f0db2a42f677f997dad55995c4c794b7dc4fd682
lamnmph <minhlam021104@gmail.com>
2024-11-04 20:53:31 +0700

    new

--- client/router.php
+++ client/router.php
@@ -4,7 +4,6 @@ class Router {
     public function add($path, $controller, $method) {
         $this->routes[$path] = [$controller, $method];
     }
-
     public function dispatch($path) {
         if (isset($this->routes[$path])) {
             call_user_func([new $this->routes[$path][0], $this->routes[$path][1]]);
