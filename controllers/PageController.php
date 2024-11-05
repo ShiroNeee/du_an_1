fa1ea4b253fd0eb0dd67fa8f99535b82f05d835d
ShiroNeee <kuramanguyen1@gmail.com>
2024-11-05 21:19:36 +0700

    abc

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -1,6 +1,7 @@
 <?php
 class PageController
 {
+    // cái này để hiển thị muốn thay đổi thì thay mỗi phần main bằng một file khác để hiển thị đúng phần của nó còn đâu giữ header và footer
     public function index()
     {
         require_once '../client-page/views/header.php';
