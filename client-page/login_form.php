d532a29562bc3d7d1f6c48c574a026c18d2c04d4
ShiroNeee <kuramanguyen1@gmail.com>
2024-11-06 16:44:02 +0700

    abcd

--- client-page/login_form.php
+++ client-page/login_form.php
@@ -1,30 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng nhập</title>
-</head>
 <body>
-    <h2>Đăng nhập</h2>
-    <form action="?act=handle-login" method="POST">
+    <div class="banner-content">
+        <div>
+            <h2>Đăng nhập</h2>
+        </div>
+        <div>
+            <form action="?act=handle-login" method="POST">
 
-        <label for="email">Email:</label>
-        <input type="email" name="email" placeholder="Nhập email">
-        <span style="color: red;">
-            <?= !empty($_SESSION['error']['email']) ? $_SESSION['error']['email'] : '' ?>
-        </span>
-        <br>
+                <label for="email">Email:</label>
+                <input type="email" name="email" placeholder="Nhập email">
+                <span style="color: red;">
+                    <?= !empty($_SESSION['error']['email']) ? $_SESSION['error']['email'] : '' ?>
+                </span>
+                <br>
 
-        <label for="mat_khau">Mật khẩu:</label>
-        <input type="password" name="mat_khau" placeholder="Nhập mật khẩu">
-        <span style="color: red;">
-            <?= !empty($_SESSION['error']['mat_khau']) ? $_SESSION['error']['mat_khau'] : '' ?>
-        </span>
-        <br>
+                <label for="mat_khau">Mật khẩu:</label>
+                <input type="password" name="mat_khau" placeholder="Nhập mật khẩu">
+                <span style="color: red;">
+                    <?= !empty($_SESSION['error']['mat_khau']) ? $_SESSION['error']['mat_khau'] : '' ?>
+                </span>
+                <br>
 
-        <button type="submit">Đăng nhập</button>
-    </form>
+                <button type="submit">Đăng nhập</button>
+            </form>
+        </div>
 
+    </div>
 </body>
-</html>
