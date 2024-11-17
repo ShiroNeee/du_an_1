3219c7e529ee4073b04a8407629430f3ffb94bc0
ShiroNeee <kuramanguyen1@gmail.com>
2024-11-17 11:28:24 +0700

    sửa lại file, đây là file connect database không xử lý code ở đây

--- common/function.php
+++ common/function.php
@@ -23,62 +23,6 @@ function connectDB()
     }
 }
 
-// function pdo_execute($sql){
-//     $sql_args=array_slice(func_get_args(),1);
-//     try{
-//         $conn=connectDB();
-//         $stmt=$conn->prepare($sql);
-//         $stmt->execute($sql_args);
-
-//     }
-//     catch(PDOException $e){
-//         throw $e;
-//     }
-//     finally{
-//         unset($conn);
-//     }
-// }
-function pdo_execute($sql) {
-    // Lấy tất cả tham số sau `$sql`
-    $sql_args = array_slice(func_get_args(), 1);
-    try {
-        $conn = connectDB();
-        $stmt = $conn->prepare($sql);
-        $stmt->execute($sql_args);
-    } catch (PDOException $e) {
-        throw $e;
-    } finally {
-        unset($conn);
-    }
-}
-
-
-// truy vấn nhiều dữ liệu
-function pdo_query($sql){
-    $sql_args=array_slice(func_get_args(),1);
-
-    try{
-        $conn=connectDB();
-        $stmt=$conn->prepare($sql);
-        $stmt->execute($sql_args);
-        $rows=$stmt->fetchAll();
-        return $rows;
-    }
-    catch(PDOException $e){
-        throw $e;
-    }
-    finally{
-        unset($conn);
-    }
-}
-
-// truy vấn  1 dữ liệu
-function pdo_query_one($sql, $params = []) {
-    $conn = connectDB();
-    $stmt = $conn->prepare($sql);
-    $stmt->execute($params);
-    return $stmt->fetch();
-}
 ?>
 
 
